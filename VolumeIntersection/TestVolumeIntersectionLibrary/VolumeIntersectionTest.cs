4bd869ba2f5d362e374572add8737ce6fd90a14a
D0miq <dpoch@email.cz>
2021-01-11 13:35:21 +0100

    2D test

--- VolumeIntersection/TestVolumeIntersectionLibrary/VolumeIntersectionTest.cs
+++ VolumeIntersection/TestVolumeIntersectionLibrary/VolumeIntersectionTest.cs
@@ -11,26 +11,23 @@ namespace TestVolumeIntersection
         {
             var vertices = new List<Vertex>()
             {
-                new Vertex(0, 0, 0),
-                new Vertex(1, 0, 0),
-                new Vertex(0, 1, 0),
-                new Vertex(0, 0, 1)
+                new Vertex(0, 0),
+                new Vertex(1, 0),
+                new Vertex(0, 1)
             };
 
             var triangles = new List<Tetrahedron>()
             {
-                new Tetrahedron(0, 1, 2, 3)
+                new Tetrahedron(0, 1, 2)
             };
 
             var generators = new List<Vertex>()
             {
-                new Vertex(1, 0, 0) { Index = 0 },
-                new Vertex(0, 0, 0) { Index = 1 },
-                new Vertex(-1, 0, 0) { Index = 2 },
-                new Vertex(0, 1, 0) { Index = 3 },
-                new Vertex(0, -1, 0) { Index = 4 },
-                new Vertex(0, 0, 1) { Index = 5 },
-                new Vertex(0, 0, -1) { Index = 6 }
+                new Vertex(1, 0) { Index = 0 },
+                new Vertex(0, 0) { Index = 1 },
+                new Vertex(-1, 0) { Index = 2 },
+                new Vertex(0, 1) { Index = 3 },
+                new Vertex(0, -1) { Index = 4 },
             };
 
             var volumeData = VolumeIntersection.VolumeIntersection.Intersect(vertices, triangles, generators);
