a4aa862540a46626347e7437bd2c53c59154a5c7
D0miq <dpoch@email.cz>
2021-01-18 23:39:27 +0100

    Intersection example

---
+++ VolumeIntersection/VolumeIntersectionExample/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VolumeIntersection;
+using VolumeIntersection.SliceVisualisation;
+
+namespace VolumeIntersectionExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var parser = new Parser();
+            var generators = new List<Vertex>();
+
+            using (var streamReader = new StreamReader("export000.txt"))
+            {
+                int i = 0;
+                string line;
+                while ((line = streamReader.ReadLine()) != null)
+                {
+                    var vertex = parser.ParseVertex(line);
+                    vertex.Index = i;
+                    generators.Add(vertex);
+                    i++;
+                }
+            }
+
+            var voronoiData = VolumeData<Vertex>.FromVoronoi(generators);
+            var bitmap = new System.Drawing.Bitmap(800, 800);
+            VolumeSlicer.Slice(bitmap, 2, 0.4, voronoiData);
+            bitmap.Save("export000.png");
+
+            var reader = new TetrahedralizationReader();
+            var tetrahedralization = reader.Read("example.dat");
+
+            for (int i = 0; i < tetrahedralization.Indices.Count; i++)
+            {
+                var indices = tetrahedralization.Indices[i].Indices;
+
+                for (int j = 0; j < indices.Length; j++)
+                {
+                    indices[j] = indices[j] - 1;
+                }
+            }
+
+            var tetrahedralizationData = VolumeData<Vertex>.FromTriangulation(tetrahedralization.Vertices, tetrahedralization.Indices);
+            bitmap = new System.Drawing.Bitmap(800, 800);
+            VolumeSlicer.Slice(bitmap, 2, 0.4, tetrahedralizationData);
+            bitmap.Save("example.png");
+
+            var volumeData = VolumeIntersection<Vertex>.Intersect(tetrahedralization.Vertices, tetrahedralization.Indices, generators);
+
+            volumeData.BoundingBox = tetrahedralizationData.BoundingBox;
+
+            bitmap = new System.Drawing.Bitmap(800, 800);
+            VolumeSlicer.Slice(bitmap, 2, 0.4, volumeData);
+            bitmap.Save("intersectExample.png");
+        }
+    }
+}
---
+++ VolumeIntersection/VolumeIntersectionExample/TetrahedralizationReader.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+
+namespace VolumeIntersectionExample
+{
+    class TetrahedralizationReader
+    {
+        private Parser parser;
+
+        public TetrahedralizationReader()
+        {
+            parser = new Parser();
+        }
+
+        public Tetrahedralization Read(string path)
+        {
+            using (var streamReader = new StreamReader(path))
+            {
+                // Read vertices.
+                int vertexCount = this.parser.ParseInt(streamReader.ReadLine());
+                var vertices = this.ReadVertices(streamReader, vertexCount);
+
+                // Read tetrahedra.
+                int tetrahedraCount = this.parser.ParseInt(streamReader.ReadLine());
+                var tetrahedra = this.ReadTetrahedra(streamReader, tetrahedraCount);
+
+                return new Tetrahedralization()
+                {
+                    Vertices = vertices,
+                    Indices = tetrahedra
+                };
+            }
+        }
+
+        /// <summary>
+        /// Reads a list of vertices from a given text stream.
+        /// </summary>
+        /// <param name="streamReader">The text stream.</param>
+        /// <param name="vertexCount">A number of vertices.</param>
+        /// <returns>The list of vertices.</returns>
+        private List<Vertex> ReadVertices(StreamReader streamReader, int vertexCount)
+        {
+            List<Vertex> vertices = new List<Vertex>(vertexCount);
+            for (int i = 0; i < vertexCount; i++)
+            {
+                vertices.Add(this.parser.ParseVertex(streamReader.ReadLine()));
+            }
+
+            return vertices;
+        }
+
+        /// <summary>
+        /// Reads a list of tetrahedra from a given text stream.
+        /// </summary>
+        /// <param name="streamReader">The text stream</param>
+        /// <param name="vectorsCount">A number of tetrahedra.</param>
+        /// <returns>The list of tetrahedra.</returns>
+        private List<Tetrahedron> ReadTetrahedra(StreamReader streamReader, int tetrahedraCount)
+        {
+            List<Tetrahedron> tetrahedra = new List<Tetrahedron>(tetrahedraCount);
+            for (int i = 0; i < tetrahedraCount; i++)
+            {
+                int[] line = this.parser.ParseIntVector(streamReader.ReadLine());
+                tetrahedra.Add(new Tetrahedron(line[0], line[1], line[2], line[3]));
+            }
+
+            return tetrahedra;
+        }
+
+        //private void ReadNeighbors(StreamReader streamReader, int neigborsCount, List<Tetrahedron> tetrahedra)
+        //{
+        //    for (int i = 0; i < neigborsCount; i++)
+        //    {
+        //        int[] line = this.ParseIntVector(streamReader.ReadLine());
+
+        //        var tempNeighbors = new List<Tetrahedron>(line.Length);
+        //        for (int j = 0; j < line.Length; j++)
+        //        {
+        //            if (line[j] >= 0)
+        //            {
+        //                tempNeighbors.Add(tetrahedra[line[j]]);
+        //            }
+        //        }
+
+        //        tetrahedra[i].Neighbors = tempNeighbors;
+        //    }
+        //}
+    }
+}
